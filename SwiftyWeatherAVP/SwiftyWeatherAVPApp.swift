e3e803106fde4f2e084fd8c75f2bf4aefdef03e7
Bob Witmer <bwitmer@golden.net>
2025-09-08 20:03:53 -0400

    Initial Commit

---
+++ SwiftyWeatherAVP/SwiftyWeatherAVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyWeatherAVPApp.swift
+//  SwiftyWeatherAVP
+//
+//  Created by Bob Witmer on 2025-09-08.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyWeatherAVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
